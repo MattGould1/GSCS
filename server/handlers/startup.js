2337a90601c68b0d711d59714ce0adbdce1550fe
Matt Gould <mattg12323@bitbucket.org>
2016-04-20 10:18:42 +0100

    admin view all boards

--- server/handlers/startup.js
+++ server/handlers/startup.js
@@ -2,9 +2,25 @@ module.exports = {
 	init: function (cUser, ChatRoom, Excel, ChatMessage, User, Word, socket) {
         //@TODO implement better method of sending data, atm chat should be prioritised look @FUTURE
         //connection data
-        ChatRoom.find({ location: cUser.location, department: cUser.department }).populate({ path: '_messages', options: { limit: 40, sort: { 'created': -1 } }}).exec(function (err, chatrooms) {
+        if (cUser.admin) {
+            var ChatRoomQuery = ChatRoom.find({})
+                                    .populate({ path: '_messages', options: { limit: 40, sort: { 'created': -1 }}});
+            var ExcelQuery = Excel.find({})
+                                  .populate('user')
+            var WordQuery = Word.find({});
+        } else {
+            var ChatRoomQuery = ChatRoom.find({ location: cUser.location, department: cUser.department })
+                                    .populate({ path: '_messages', options: { limit: 40, sort: { 'created': -1 }}});
+            var ExcelQuery = Excel.find({ location: cUser.location, department: cUser.department })
+                                  .populate('user')
+            var WordQuery = Word.find({ location: cUser.location, department: cUser.department })
+        }
+
+
+
+        ChatRoomQuery.exec(function (err, chatrooms) {
             if (err) { console.log('socketio error finding chatrooms' + err); socket.emit('data', false); return false; }
-            Excel.find({ location: cUser.location, department: cUser.department }).populate('user').exec(function (err, excelsheets) {
+            ExcelQuery.exec(function (err, excelsheets) {
                 if (err) { console.log('socketio error finding excelsheets' + err); socket.emit('data', false); return false; }
                 ChatMessage.find()
                     .where('_to').equals(socket.decoded_token._id)
@@ -14,8 +30,7 @@ module.exports = {
                         User.find()
                             .select('username status email online lastlogin')
                             .exec( function (err, names) {
-                                Word.find({ location: cUser.location, department: cUser.department })
-                                    .exec( function (err, words) {
+                                WordQuery.exec( function (err, words) {
                                         //emit data
                                         var data = {
                                             chatrooms: chatrooms,
